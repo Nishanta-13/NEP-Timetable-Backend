26952bcee56764e4e5c6fae1fb8756fac225709f
Nishanta Kamal Baruah <nishantakamalbaruah@Mac.lan>
2025-10-14 01:06:59 +0530

    Final Backend

---
+++ prisma/migrations/20251006195803_admin_mail_unique/migration.sql
@@ -0,0 +1,13 @@
+/*
+  Warnings:
+
+  - A unique constraint covering the columns `[adminMail]` on the table `User` will be added. If there are existing duplicate values, this will fail.
+  - Made the column `professorId` on table `User` required. This step will fail if there are existing NULL values in that column.
+
+*/
+-- AlterTable
+ALTER TABLE "public"."User" ADD COLUMN     "professorBranch" TEXT,
+ALTER COLUMN "professorId" SET NOT NULL;
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_adminMail_key" ON "public"."User"("adminMail");
